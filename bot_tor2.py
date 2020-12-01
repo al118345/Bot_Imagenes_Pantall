d870c49c286eddb3624ae320c795c44b7b2e22ed
ruben <rubenpeib@gmail.com>
2020-12-01 16:10:37 +0100

    comit para añadir el lector de imagenes

--- bot_tor2.py
+++ bot_tor2.py
@@ -76,34 +76,39 @@ def intento():
 
 intento()
 while True:
-    time.sleep(10000)
     time.sleep(3)
     search = Search("tor/problema.png")
     pos = search.imagesearch()
     if pos[0] != -1:
          intento()
     else:
-        time.sleep(5)
-        search = Search("tor/no_gracias_"
-                        "2.png")
+        search = Search("tor/error2.png")
         pos = search.imagesearch()
         if pos[0] != -1:
-            pyautogui.moveTo(pos[0], pos[1])
-            time.sleep(1)
-            pyautogui.click()
-            pyautogui.press('enter', interval=0.25)
-            time.sleep(5)
-            print('esta')
+            intento()
         else:
-            print("image not found")
+            time.sleep(5)
+            search = Search("tor/no_gracias_"
+                            "2.png")
+            pos = search.imagesearch()
+            if pos[0] != -1:
+                pyautogui.moveTo(pos[0], pos[1])
+                time.sleep(1)
+                pyautogui.click()
+                pyautogui.press('enter', interval=0.25)
+                time.sleep(5)
+                print('esta')
+            else:
+                print("image not found")
 
-        search = Search("tor/acepto.png")
-        pos = search.imagesearch()
-        if pos[0] != -1:
-            pyautogui.moveTo(pos[0], pos[1])
-            time.sleep(1)
-            pyautogui.click()
-            pyautogui.press('enter', interval=0.25)
-            time.sleep(5)
-        else:
-            print("image not found")
+            search = Search("tor/acepto.png")
+            pos = search.imagesearch()
+            if pos[0] != -1:
+                pyautogui.moveTo(pos[0], pos[1])
+                time.sleep(1)
+                pyautogui.click()
+                pyautogui.press('enter', interval=0.25)
+                time.sleep(5)
+            else:
+                print("image not found")
+    time.sleep(10000)
